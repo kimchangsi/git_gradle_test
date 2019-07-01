b31c35522bd9078f9234c3beae5e4177e9b09ad4
김창민 <kkccmm456@naver.com>
2019-07-01 15:01:37 +0900

    System.out.println("earth's end Dancehall"); 추가

--- src/main/java/git_gradle_test/HellowWord.java
+++ src/main/java/git_gradle_test/HellowWord.java
@@ -4,5 +4,6 @@ public class HellowWord {
 	public static void main(String[] args) {
 		System.out.println("Hellow World");
 		System.out.println("world's end Dancehall");
+		System.out.println("earth's end Dancehall");
 	}
 }
